4555a15658886303c7950cefe0a42b6bed600184
Lggslemi <96556081+Lggslemi@users.noreply.github.com>
2025-12-06 17:07:15 -0500

    Delete tutorias-hexagonal directory

--- tutorias-hexagonal/src/main/java/com/jcaa/hexagonal/adapter/databases/sql/repository/PasswordResetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jcaa.hexagonal.adapter.databases.sql.repository;
-
-import com.jcaa.hexagonal.adapter.databases.sql.entity.PasswordResetEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface PasswordResetRepository extends JpaRepository<PasswordResetEntity, Integer> {
-    Optional<PasswordResetEntity> findByToken(String token);
-}
--- tutorias-hexagonal/src/main/java/com/jcaa/hexagonal/adapter/databases/sql/repository/TutoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jcaa.hexagonal.adapter.databases.sql.repository;
-
-import com.jcaa.hexagonal.adapter.databases.sql.entity.TutoriaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TutoriaRepository extends JpaRepository<TutoriaEntity, String> {
-}
